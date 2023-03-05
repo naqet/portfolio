2f8670a69c61da9c32c52691d026f37831ef40f7
Szymon Szadkowski <szymon.tomasz.szadkowski@gmail.com>
2023-03-05 10:30:47 +0100

    Quickfix for build script

--- src/components/CanvasBg.tsx
+++ src/components/CanvasBg.tsx
@@ -2,23 +2,23 @@ import { initCanvas, clearEventListeners } from "../scripts/renderCanvas";
 import { useEffect } from "react";
 
 const CanvasBg = () => {
-	useEffect(() => {
-		initCanvas();
-		return () => clearEventListeners();
-	}, []);
+  useEffect(() => {
+    initCanvas();
+    return () => clearEventListeners();
+  }, []);
 
-	return (
-		<canvas
-			id="canvas-bg"
-			style={{
-				position: "fixed",
-				inset: 0,
-				zIndex: -1,
-				width: "100vw",
-				height: "100vh",
-			}}
-		/>
-	);
+  return (
+    <canvas
+      id="canvas-bg"
+      style={{
+        position: "fixed",
+        inset: 0,
+        zIndex: -1,
+        width: "100vw",
+        height: "100vh",
+      }}
+    />
+  );
 };
 
 export default CanvasBg;
