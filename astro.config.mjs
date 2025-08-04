6f3ea83de7d20dbbef2f29a3693f9c74c4bac797
naqet <naqet8@gmail.com>
2025-08-04 17:50:39 +0200

    feat: prepare for self hosting and animation on all pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://naqet.github.io",
-    base: "/portfolio",
     server: {
         port: 3000,
     },
